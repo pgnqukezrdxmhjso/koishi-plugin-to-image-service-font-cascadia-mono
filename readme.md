9deb1efa5d26af549494ba0001f6deb3997958c0
173gvQY8MFaDk9Jt <g@v.q>
2025-01-10 19:04:50 +0800

    first commit

--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-vercel-satori-png-service-font-cascadia-mono?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-vercel-satori-png-service-font-cascadia-mono)
 
 Provide Cascadia Mono fonts for the vercel-satori-png-service
+
+---
+
+[cascadia-code](https://github.com/microsoft/cascadia-code)
+
+![example.png](example.png)
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,62 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service } from "koishi";
+import type { Font, FontWeight, FontStyle } from "satori";
+import fs from "node:fs/promises";
+import path from "node:path";
+// noinspection ES6UnusedImports
+import {} from "koishi-plugin-vercel-satori-png-service";
 
-export const name = 'vercel-satori-png-service-font-cascadia-mono'
+const serviceName = "vercelSatoriPngServiceFontCascadiaMono";
 
-export interface Config {}
+class VercelSatoriPngServiceFontCascadiaMono extends Service {
+  private _ctx: Context;
+  constructor(
+    ctx: Context,
+    config: VercelSatoriPngServiceFontCascadiaMono.Config,
+  ) {
+    super(ctx, serviceName);
+    this._ctx = ctx;
+  }
 
-export const Config: Schema<Config> = Schema.object({})
+  protected async start(): Promise<void> {
+    await this.initFonts();
+  }
+  private async initFonts() {
+    const fonts: Font[] = [];
+    for (let i = 0; i < this.fontNames.length; i++) {
+      const fontName = this.fontNames[i];
+      for (let j = 0; j < 2; j++) {
+        fonts.push({
+          name: "Cascadia Mono",
+          weight: ((i + 2) * 100) as FontWeight,
+          style: ["normal", "italic"][j] as FontStyle,
+          data: await fs.readFile(
+            path.join(
+              __dirname,
+              `../fonts/CascadiaMono-${j === 0 || fontName !== "Regular" ? fontName : ""}${["", "Italic"][j]}.ttf`,
+            ),
+          ),
+        });
+      }
+    }
+    this._ctx.vercelSatoriPngService.addFont(fonts);
+  }
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  private fontNames: string[] = [
+    "ExtraLight",
+    "Light",
+    "SemiLight",
+    "Regular",
+    "SemiBold",
+    "Bold",
+  ];
 }
+
+namespace VercelSatoriPngServiceFontCascadiaMono {
+  export const inject = ["vercelSatoriPngService"];
+
+  export const usage =
+    '<a target="_blank" href="https://github.com/microsoft/cascadia-code">cascadia-code</a>';
+  export interface Config {}
+  export const Config: Schema<Config> = Schema.object({});
+}
+export default VercelSatoriPngServiceFontCascadiaMono;
